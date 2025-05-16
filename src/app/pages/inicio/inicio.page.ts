3100ed6bac62e8ac24d7510d533528166c3c14e1
Edwin <ingedwinarg@gmail.com>
2025-05-15 20:15:55 -0500

    second commit

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,40 +1,37 @@
-import { Component, OnInit } from '@angular/core'; 
-import { ComponentsModule } from '../../components/components.module'; 
- 
+import { Component, ComponentRef, OnInit } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
+
 //crear una interface para definir propiedades que el componente lista va a tener 
-interface Componente { 
-  icon: string; 
-  name: string; 
-  redirectTo: string; 
-} 
- 
-@Component({ 
-  selector: 'app-inicio', 
-  templateUrl: './inicio.page.html', 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
-  standalone: false 
-}) 
-export class InicioPage implements OnInit { 
- 
+  standalone: false
+})
+export class InicioPage implements OnInit {
   //crear nuevo listado 
- 
+
   //componentes: any[] = []; 
-  componentes: Componente[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: 'alert-circle-outline',
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-  ];   
- 
-  constructor() { } 
- 
-  ngOnInit() { 
-  } 
- 
+  componentes!: Observable<Componente[]>;
+
+  constructor(private menuCtrl: MenuController,
+    private dataService: DataService) { }
+    
+  ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
+  }
+
+  mostrarMenu() {
+    this.menuCtrl.open('first');
+  }
+
 } 
